913e73f05a08c032d0ea350e8d577be0d6d26253
Kota Ogawa <k.ogawa.biz406@gmail.com>
2021-12-19 14:18:19 +0900

    🚧 class-validator練習

--- server/validators/index.ts
+++ server/validators/index.ts
@@ -1,4 +1,4 @@
-import { MinLength } from 'class-validator'
+import { IsBoolean, IsOptional, IsString, Length, MinLength } from 'class-validator'
 
 export class LoginBody {
   @MinLength(2)
@@ -7,3 +7,24 @@ export class LoginBody {
   @MinLength(4)
   pass: string
 }
+
+
+// class-validator というライブラリとても便利
+export class TaskBody {
+  @IsString({ message: '文字列で入力してください' })
+  @Length(1, 5, { message: '1-5文字でOK' })
+  label: string
+
+  @IsOptional()
+  @IsBoolean()
+  done: boolean
+
+  @IsString()
+  body: string
+}
+
+// 下のような感じで型指定にも使える！
+// post: {
+//   reqBody: TaskBody // ← ココ！
+//   resBody: Task
+// }
