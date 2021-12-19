06aa7ba3d707f29d26444def07276b214c21caff
Kota Ogawa <k.ogawa.biz406@gmail.com>
2021-12-19 22:00:26 +0900

    🚧 関数名の修正

--- server/api/threads/controller.ts
+++ server/api/threads/controller.ts
@@ -1,9 +1,9 @@
 import { defineController } from './$relay'
-import { getThreads } from '$/service/threads'
+import { fetchThreads } from '$/service/threads'
 
 // ルーティングの役割（HTTPメソッドと返り値の定義）
-export default defineController({ getThreads }, ({ getThreads }) => ({
+export default defineController({ fetchThreads }, ({ fetchThreads }) => ({
   get: async () => {
-    return { status: 200, body: await getThreads() }
+    return { status: 200, body: await fetchThreads() }
   }
 }))
--- server/service/threads.ts
+++ server/service/threads.ts
@@ -3,7 +3,7 @@ import { prisma } from '$/lib/prismaClient'
 import type { Thread } from '$prisma/client'
 
 // DBとの接続、DBアクセスメソッドの定義
-export const getThreads = depend(
+export const fetchThreads = depend(
   { prisma: prisma as { thread: { findMany(): Promise<Thread[]> } } },
   async ({ prisma }) => await prisma.thread.findMany()
 )
